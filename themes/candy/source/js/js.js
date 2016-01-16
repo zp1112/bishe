7ad3d4a755e021c5669f4b1d82995dc9ac84c261
zp1112 <1192979260@qq.com>
2016-01-16 16:17:45 +0800

    bg

--- themes/candy/source/js/js.js
+++ themes/candy/source/js/js.js
@@ -2,11 +2,14 @@ window.onload=function(){
   var s = skrollr.init();
   window.onscroll=function(){
     var top=document.getElementsByClassName('top')[0];
+    var img1=document.getElementsByClassName('img1')[0];
     if(window.scrollY>0){
       top.className='top hover';
+      img1.className='img1 hide';
     }
     else{
       top.className='top';
+      img1.className='img1'
     }
 
 
